e0991ff6e203cf10a52a8a298cf182da7e309524
harshavardhanmantri <harshavardhanmantri935@gmail.com>
2023-02-20 14:34:47 +0530

    completed dashboard -no learning course

---
+++ linkedlearning/src/components/AdvertBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="row" id="secondh">
+        <h5>Advertisment</h5>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+ export default{
+        name:'AdvertBar',
+    }
+</script>
+
+<style>
+#secondh{
+        box-shadow: 4px 4px 4px 4px #80808099;
+        background-color: #d1cbcb4d;
+        border-radius: 0.5vw;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        margin: 1vw 0vw ;
+        height:10vw;
+        width:80vw;
+}
+
+</style>
---
+++ linkedlearning/src/components/FindBar.vue
@@ -0,0 +1,29 @@
+<template>
+    
+    <div class="row" id="firstdh">
+        <input @click="getcourse" type="text" class="form-control" id="searchdh" placeholder="    Search For Courses"/>
+    </div>
+    
+</template>
+
+<script>
+    export default{
+        name:'FindBar',
+    }
+</script>
+<style>
+    #firstdh{
+        justify-content: center;
+        align-items: center;
+        margin: 1vw 0vw ;
+
+    }
+    #searchdh{
+        height: 2vw;
+        width:30vw;
+        border-radius:0.6vw;
+        box-shadow: 2px 2px 2px 2px #80808099;
+        background-color: #d1cbcb4d;
+    }
+
+</style>
